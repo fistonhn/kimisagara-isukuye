f55cb161ddf5687084f641099070856806d97e1a
fistonhn <fistonhn@gmail.com>
2023-01-10 12:54:33 +0200

    avoid limit

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.json());
 
 app.use(cors({origin: "*"}));
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
 app.use("/api/", postRoute);
 
 
